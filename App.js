8971486e3853bb9ea202f5784102e3f8a7027973
tsaglash <162251233+tsaglash@users.noreply.github.com>
2024-06-11 11:51:37 +0800

    H
    H

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import Emoji from "./components/Boox.js";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="emoji">
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+        <Emoji />
+      </div>
+      ;
+    </div>
+  );
+};
+
+export default App;
